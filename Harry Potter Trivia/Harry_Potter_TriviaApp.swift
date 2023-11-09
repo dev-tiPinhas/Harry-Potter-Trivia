d4ee16f590c11a09cab0e0e8a4b80fd5b492f492
Tiago Pinheiro <tiago.pinheiro@itsector.pt>
2023-11-09 13:51:44 +0000

    Initial Commit

---
+++ Harry Potter Trivia/Harry_Potter_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harry_Potter_TriviaApp.swift
+//  Harry Potter Trivia
+//
+//  Created by Tiago Pinheiro on 09/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Harry_Potter_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
